f897b42be4ddedfaa88e35c7bb1ba9f6680d10fc
Ayman Salah <80077545+AymanSalah1994@users.noreply.github.com>
2022-09-14 11:16:29 +0200

    Cart View Design Done

--- resources/views/customer/store/view-cart.blade.php
+++ resources/views/customer/store/view-cart.blade.php
@@ -11,7 +11,37 @@
         </nav>
     </div>
 
-    
+    <div class="container">
+        @foreach ($currentCartItems as $cartItem)
+            <div class="card shadow product_data mb-3">
+                <div class="card-body">
+                    <div class="row align-items-center justify-content-center">
+                        <div class="col-md-4">
+                            <img src="{{ Storage::url($cartItem->product->product_picture) }}" class="image-responsive"
+                                width="25%" alt="">
+                        </div>
+                        <div class="col-md-4">
+                            <h3>{{ $cartItem->product->name }}</h3>
+                        </div>
+                        <div class="col-md-2">
+                            <label for="">Quantity</label>
+                            <div class="input-group text-center mb-3">
+                                <span class="input-group-text decrement-btn">-</span>
+                                <input type="text" name="" value="{{ $cartItem->quantity }}"
+                                    class="form-control quantity-input">
+                                <span class="input-group-text increment-btn">+</span>
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <button type="button" class="btn btn-danger deleteFromCartBtn">
+                                Delete <i class="bi bi-trash3"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
 
     <br>
 @endsection
