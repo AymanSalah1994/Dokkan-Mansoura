fac6f1f11b48456afb6039e337f801dc10cca39b
Ayman Salah <80077545+AymanSalah1994@users.noreply.github.com>
2022-09-27 12:21:49 +0200

    V 0.5

--- app/Http/Controllers/Customer/Store/SearchController.php
+++ app/Http/Controllers/Customer/Store/SearchController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Customer\Store;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Customer\SearchRequest;
 use App\Models\Category;
 use App\Models\Product;
 use App\Models\User;
