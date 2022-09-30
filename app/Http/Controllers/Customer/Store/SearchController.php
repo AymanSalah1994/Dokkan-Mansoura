b906cba2d992f16852d4f02b23d60e73cffaae64
Ayman Salah <80077545+AymanSalah1994@users.noreply.github.com>
2022-10-01 00:46:15 +0200

    Search inhancement

--- app/Http/Controllers/Customer/Store/SearchController.php
+++ app/Http/Controllers/Customer/Store/SearchController.php
@@ -5,19 +5,41 @@ namespace App\Http\Controllers\Customer\Store;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use App\Models\Product;
+use App\Models\SearchHistory;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SearchController extends Controller
 {
     public function index(Request $request)
     {
+
         $all_categories  = Category::all();
         $all_merchants   = User::where('role_as', '2')->get();
         $allProducts = Product::orderBy('updated_at', 'DESC')->LowHigh()->SelectCategory()->SelectMerchant()->SearchWord()->MiniPrice()->MaxPrice()->with('user')->paginate(10);
         $sent_data = ['allProducts', 'all_categories', 'all_merchants'];
-        // TODO :
-        // Order Them By Newest/Lates First  ;
+
+        if ($request->search_word) {
+            $this->storeHistory($request);
+        }
         return view('customer.store.search', compact($sent_data));
+    }
+
+
+    public function storeHistory(Request $request)
+    {
+        $search_history = new SearchHistory();
+        $search_history->search_word = $request->search_word;
+        $search_history->ip = $request->ip();
+        if (Auth::check()) {
+            $search_history->id_u = $request->user()->id;
+            $search_history->name = $request->user()->first_name;
+        } else {
+            $search_history->id_u = null;
+            $search_history->name = "Guest";
+        }
+
+        $search_history->save();
     }
 }
