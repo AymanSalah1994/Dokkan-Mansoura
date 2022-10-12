5f63ecafe61de1f1799d76ad5a0c030085806682
Ayman Salah <80077545+AymanSalah1994@users.noreply.github.com>
2022-10-12 22:42:51 +0200

    Refactor

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\Admin\OrderController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MailController;
 
 Route::get('send-mail', [MailController::class, 'index'])->name('notify.with.email');
 // TODO
-// Make it Post With Moore Complexity 
+// Make it Post With Moore Complexity
+// Route::Controller(OrderController::class)->group(function () {
+// });
